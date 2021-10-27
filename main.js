cd840788c81ac5aa4b7d5d107b4b57fe4a7d98fc
Satyam Pariyar <pariyarsatyam1@gmail.com>
2021-10-27 21:50:54 +0530

    Minor changes

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
+window.addEventListener('keydown', playSound);
+
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
-    if(!audio) return;
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     audio.currentTime = 0;
     audio.play();
-    key.classList.toggle('playing');
+    if(!audio) return;
+    key.classList.add('playing');
 };
 
-function removeTransition(e) {
-    if(e.propertyName !== 'transform') return;
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e){
+    if(e.propertyName !== "transform") return;
     this.classList.remove('playing');
 };
-
-window.addEventListener('keydown', playSound)
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
